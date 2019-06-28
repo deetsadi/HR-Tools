adc3ea45ba839923d032761855003785780a3fb2
Sean Kurian <Sean.Kurian@ibm.com>
2019-06-28 13:48:25 -0400

    Update attrition.py

--- attrition.py
+++ attrition.py
@@ -7,8 +7,18 @@ Created on Wed Jun 26 09:36:23 2019
 
 import pandas as pd
 from sklearn import preprocessing 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv(r'C:\Users\SEANKurian\Desktop\attritionSheet.csv')
-print(df)
+
+#Turning all values into scaled numbers
 df_encoded = df.apply(preprocessing.LabelEncoder().fit_transform)
-print (df_encoded)
+
+predictors = df.drop(columns=['Attrition'])
+target = df['Attrition'].values
+
+predictors_train, predictors_test, target_train, target_test = train_test_split(predictors, target, test_size=0.2, random_state=1, stratify=target)
+
+model = KNeighborsClassifier(n_neighbors=3)
+#model.fit()
