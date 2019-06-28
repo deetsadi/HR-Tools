0ddb54de7d67b980df1b4aa69fcaf1db30e85385
Sean Kurian <Sean.Kurian@ibm.com>
2019-06-28 14:16:20 -0400

    Update attrition.py

--- attrition.py
+++ attrition.py
@@ -5,20 +5,28 @@ Created on Wed Jun 26 09:36:23 2019
 @author: SEANKurian
 """
 
+#Was using train test split on last commit, found cross val score is more accurate
+#Plan on using hyper tuning to determine optimal number of neighbors, to be implemented
+
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 
 df = pd.read_csv(r'C:\Users\SEANKurian\Desktop\attritionSheet.csv')
 
 #Turning all values into scaled numbers
 df_encoded = df.apply(preprocessing.LabelEncoder().fit_transform)
 
-predictors = df.drop(columns=['Attrition'])
-target = df['Attrition'].values
+predictors = df_encoded.drop(columns=['Attrition'])
+target = df_encoded['Attrition'].values
 
-predictors_train, predictors_test, target_train, target_test = train_test_split(predictors, target, test_size=0.2, random_state=1, stratify=target)
 
 model = KNeighborsClassifier(n_neighbors=3)
-#model.fit()
+cv_scores = cross_val_score(model, predictors, target, cv=5)
+
+
+print(cv_scores)
+print('cv_scores mean:{}'.format(np.mean(cv_scores)))
