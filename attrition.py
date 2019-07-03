7170e5a01d53b63ae0e7f70d0f3d072f61225f80
Sean Kurian <Sean.Kurian@ibm.com>
2019-07-03 08:58:10 -0400

    Update attrition.py

--- attrition.py
+++ attrition.py
@@ -4,10 +4,8 @@ Created on Wed Jun 26 09:36:23 2019
 
 @author: SEANKurian
 """
-
 #Was using train test split on last commit, found cross val score is more accurate
 #Plan on using hyper tuning to determine optimal number of neighbors, to be implemented
-
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing 
@@ -16,15 +14,12 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 
-
-
+#Turning all values into scaled numbers after reading in a spreadsheet
 df = pd.read_csv(r'C:\Users\SEANKurian\Desktop\attritionSheet.csv')
-#Turning all values into scaled numbers
 df_encoded = df.apply(preprocessing.LabelEncoder().fit_transform)
 
 predictors = df_encoded.drop(columns=['Attrition'])
 target = df_encoded['Attrition'].values
-
 pred_train, pred_test, tar_train, tar_test = train_test_split(predictors, target, test_size=0.25, random_state=1)
 
 #Determines optimal number of neighbors 
@@ -32,7 +27,6 @@ model = KNeighborsClassifier()
 param_grid = {'n_neighbors': np.arange(1, 25)}
 modelGSCV = GridSearchCV(model, param_grid, cv=5)
 modelGSCV.fit(pred_train, tar_train)
-
 
 #Optimal accuracy of ~84.6% is achieved with 20 neighbours
 print(modelGSCV.best_params_)
